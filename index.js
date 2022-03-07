3c87e4387bbf8c7c8a64a964e85c33b46493767a
Brian Serein <bserein22@gmail.com>
2022-03-07 10:12:38 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express'); 
+const cors = require('cors'); 
+const { createUser, loginUser, getUsers } = require('./src/authenticatedUsers')
+const { createCar, getCars, deleteCars, updateCars, getSingleCar } = require('./src/cars')
+const PORT = process.env.PORT || 4325
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/users", getUsers)
+app.post("/users",createUser)
+app.post("/users/login", loginUser)
+
+app.get("./cars", getCars)
+app.get('/cars/:id', getSingleCar)
+app.post("/cars", createCar);
+app.patch('/cars/:id', updateCars)
+app.delete('/cars/:id', deleteCars)
+
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT: ', PORT)
+});
---
+++ src/cars.js
@@ -0,0 +1,95 @@
+const { connectDB } = require("./dbConnect");
+
+exports.createCar = (request, response) => {
+    const addCar = {
+        make: request.body.make,
+        model: request.body.model,
+        photo: request.body.photo,
+        isPosted: false
+        // Need to get id and find a way to add the ratings *************************************
+    }
+    const db = connectDB();
+    db.collection("cars")
+    .add(addCar)
+    .then((doc) => response.status(201).send(doc.id))
+    .catch((err) => response.status(500).send(err))
+}
+
+exports.getCars = (request, response) => {
+    const db = connectDB();
+    db.collection("cars")
+    .get()
+    .then((snapshot) => {
+        const carList = snapshot.docs.map((doc) => {
+            let car = doc.data();
+            car.id = doc.id;
+            return car 
+        })
+        response.send(carList)
+    })
+    .catch((err) => response.status(500).send(err))
+}
+
+exports.updateCars = (request, response) => {
+    const db = connectDB();
+    db.collection('cars')
+    if (request.body.rating) {
+      db.collection('cars')
+        .doc(request.params.id)
+        .get()
+        .then(doc => {
+          const newRating = Number(request.body.rating)
+          let newRatings = doc.data().ratings || []
+          newRatings.push(newRating)
+          const newCount = newRatings.length
+          const newRatingsSum = newRatings.reduce((accum, curr) => accum + curr, 0)
+          const newAvgRating = newRatingsSum / newCount
+          const newRatingObj = {
+            ratings: newRatings,
+            averageRating: newAvgRating,
+            numberOfRatings: newCount,
+          }
+          db.collection('cars')
+            .doc(request.params.id)
+            .update(newRatingObj)
+            .then(() => {
+              response.status(200).send(`car ${request.params.id} was updated`)
+            })
+        })
+        .catch(err => response.status(500).send(err))
+    } else {
+      db.collection('cars')
+        .doc(request.params.id)
+        .update(request.body)
+        .then(() => {
+          this.getCars(request, response)
+        })
+        .catch(err => response.status(500).send(err))
+    }
+  }
+  
+  exports.getSingleCar = (request, response) => {
+      const db = connectDB();
+    db.collection('cars')
+      .doc(request.params.id)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          response.send(doc.data())
+        } else {
+          response.status(404).send('No such document!')
+        }
+      })
+      .catch(err => response.status(500).send(err))
+  }
+
+  exports.deleteCars = (request, response) => {
+      const db = connectDB();
+    db.collection('cars')
+      .doc(request.params.id)
+      .delete()
+      .then(() => {
+        this.getCars(request, response)
+      })
+      .catch(err => response.status(500).send(err))
+  }
